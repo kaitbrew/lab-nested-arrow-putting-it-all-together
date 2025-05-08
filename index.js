62112be3658739f165c40505e4e011b0ce8bae5d
kait brewer <kaitlynmbrewer@gmail.com>
2025-05-07 21:35:01 -0400

    JS lab submission!

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-
-
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  let isLocked = false;
+  let loggedIn = false;
+  const passwordCheck = (passwordAttempt) => {
+    if (passwordAttempt === userInfo.password && attemptCount < 3) {
+      attemptCount = 0;
+      loggedIn = true;
+      return "Login successful";
+    } else if (passwordAttempt != userInfo.password && attemptCount < 3) {
+      attemptCount += 1;
+      return `Attempt ${attemptCount}: Login failed`;
+    } else {
+      isLocked = true;
+      return "Account locked due to too many failed login attempts";
+    }
+  };
+  return passwordCheck;
+}
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
